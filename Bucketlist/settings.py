8201ab490900af424faef96f427d7726ee9a1f50
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2021-12-13 21:45:56 -0700

    settings database

--- Bucketlist/settings.py
+++ Bucketlist/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,9 +85,12 @@ DATABASES = {
     'USER': 'ADMIN13787',
     'PASSWORD': 'Server234534',
     'HOST': 'is415-jbb-server.postgres.database.azure.com'
-
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
