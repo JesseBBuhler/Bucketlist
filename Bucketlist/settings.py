6073aea5e08de7450bd824cf78d6d4c094baa39f
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2021-12-13 21:37:22 -0700

    whatever the heck whitenoise is

--- Bucketlist/settings.py
+++ Bucketlist/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Bucketlist.urls'
@@ -126,6 +127,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'bucketlist/static')
 ]
